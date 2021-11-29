d2481cc741799bc7d54bb059e0166e71da28da6a
Chia-Yin Fan <cyfan@Chia-Yins-MacBook-Air.local>
2021-11-29 17:11:41 +0800

    add buttoms

--- src/App.js
+++ src/App.js
@@ -31,6 +31,15 @@ function App() {
       <button onClick={onSearchButtonClick=> navigate('/search')}>
         Search for Jobs
       </button>
+      <button onClick={onSearchButtonClick=> navigate('/create')}>
+        Create Jobs
+      </button>
+      <button onClick={onSearchButtonClick=> navigate('/favorite')}>
+        my favorites
+      </button>
+      <button onClick={onSearchButtonClick=> navigate('/job')}>
+        job
+      </button>
       <div>
         Pokemon Name: {pokemon.name}
       </div>
